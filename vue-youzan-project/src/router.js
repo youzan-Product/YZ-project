97663199aefc7952ef3277649cc99b1200a26019
sunyiliu <sylea142@gmail.com>
2019-11-05 18:05:50 +0800

    “静态页面完成”

--- vue-youzan-project/src/router.js
+++ vue-youzan-project/src/router.js
@@ -4,6 +4,7 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 // 引入路由文件
 import General  from '@/views/home/General .vue'
+import Indent from '@/views/indent/Indent.vue'
 
 
 // 创建所需路由
@@ -12,6 +13,10 @@ const router = new VueRouter({
         {
             path:"/home",
             component:General 
+        },
+        {
+            path:'/indent',
+            component:Indent
         }
     ]
 })
